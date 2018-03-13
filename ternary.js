317c4adb9b4eb3013951a1de29f1b109fa955e60
Allen Cheng <allencheng219@gmail.com>
2018-03-13 00:11:48 -0500

    changed ternary.rb file and added ternary.js

---
+++ ternary.js
@@ -0,0 +1,4 @@
+let zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump <= mpg * fuelLeft ? true : false;
+};
+console.log(zeroFuel(50, 25, 2));
--- ternary.rb
+++ ternary.rb
@@ -1,5 +1,4 @@
 x = 74
-x > 10 ? result = "big number" : result = "small number"
-puts result
+puts x > 10 ? "big number" : "small number"
 
 # if_this_is_a_true_value ? then_the_result_is_this : else_it_is_this
