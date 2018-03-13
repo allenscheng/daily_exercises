68d7b9a15d758d4493bdcd7f91a0ae142ad602ef
Allen Cheng <allencheng219@gmail.com>
2018-03-13 01:03:42 -0500

    refactored sumTheString

--- sumTheString.js
+++ sumTheString.js
@@ -1,15 +1,4 @@
 function sumStr(a, b) {
-  let num1 = parseInt(a);
-  let num2 = parseInt(b);
-  if (a === "" && b === "") {
-    return "0";
-  }
-  if (a === "") {
-    num1 = 0;
-  }
-  if (b === "") {
-    num2 = 0;
-  }
-  return (num1 + num2).toString();
+  return String(Number(a) + Number(b));
 }
-console.log(sumStr("1", "2"));
+console.log(sumStr("1", ""));
