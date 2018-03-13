44cf3e2b54dcc9d0a67b044591e2d2baa6a0928b
Allen Cheng <allencheng219@gmail.com>
2018-03-13 01:01:37 -0500

    changed sumTheString to account for emtpy strings

--- sumTheString.js
+++ sumTheString.js
@@ -1,6 +1,15 @@
-function sumStr(str1, str2) {
-  let num1 = parseInt(str1);
-  let num2 = parseInt(str2);
+function sumStr(a, b) {
+  let num1 = parseInt(a);
+  let num2 = parseInt(b);
+  if (a === "" && b === "") {
+    return "0";
+  }
+  if (a === "") {
+    num1 = 0;
+  }
+  if (b === "") {
+    num2 = 0;
+  }
   return (num1 + num2).toString();
 }
 console.log(sumStr("1", "2"));
