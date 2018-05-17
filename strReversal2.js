28f7d0d634234a10594400be6f4c6d46726498e3
Allen Cheng <allencheng219@gmail.com>
2018-05-16 23:04:25 -0500

    finished simple string reversal 2

---
+++ strReversal2.js
@@ -0,0 +1,13 @@
+function solve(st, a, b) {
+  return (
+    st.slice(0, a) +
+    st
+      .slice(a, b + 1)
+      .split("")
+      .reverse()
+      .join("") +
+    st.slice(b + 1)
+  );
+}
+console.log(solve("codewars", 1, 5));
+console.log(solve("FunctionalProgramming", 2, 15));
