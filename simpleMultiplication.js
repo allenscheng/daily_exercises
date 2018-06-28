32e4e5581d255d7b9118bb0478023f577fb2c32c
Allen Cheng <allencheng219@gmail.com>
2018-06-27 23:24:45 -0500

    finished Simple Multiplication

---
+++ simpleMultiplication.js
@@ -0,0 +1,8 @@
+function simpleMultiplication(num) {
+  return num % 2 === 0 ? num * 8 : num * 9;
+}
+console.log(simpleMultiplication(2));
+console.log(simpleMultiplication(1));
+console.log(simpleMultiplication(8));
+console.log(simpleMultiplication(4));
+console.log(simpleMultiplication(5));
