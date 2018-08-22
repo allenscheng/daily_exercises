19f98bf86ce41a62ae1461372d4d9db75eb05131
Allen Cheng <allencheng219@gmail.com>
2018-08-21 23:52:36 -0500

    finished Split String from CodeWars

--- splitString.js
+++ splitString.js
@@ -1,4 +1,13 @@
 function solution(str) {
-  let split = str.split("");
+  var paired = [];
+  var splitted = str.split('');
+  var length = splitted.length / 2;
+  if (str.length % 2 !== 0) {
+    splitted.push('_');
+  }
+  for (var i = 0; i < length; i++) {
+    paired.push(splitted.splice(0, 2).join(''));
+  }
+  return paired;
 }
 console.log(solution("abc"));
